75eb5ebad951b562d0c2eb91c78bc3cf923752ee
Felipe Coll <leonfelipecoll@gmail.com>
2025-07-04 22:27:24 -0300

    Configuracion de archivo index en detalle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 const app = express();
 const port = 3000;
+
+const swaggerDocument = YAML.load('./openapi.yaml');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/hello', (req, res) => {
   res.json({message:'Hello World!'});
