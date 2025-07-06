2393080149d149c0cc2dad8886180d5ed9e90cd1
Felipe Coll <leonfelipecoll@gmail.com>
2025-07-06 16:02:11 -0300

    Configuraciones en archivo .yaml

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 // Importa las dependencias necesarias
 const swaggerUi = require('swagger-ui-express');
-
 const YAML = require('yamljs');
+const OpenApiValidator = require('express-openapi-validator');
 
 const app = express();
 const port = 3000;
@@ -10,8 +10,24 @@ const port = 3000;
 // Carga el archivo OpenAPI (Swagger) en formato YAML
 // Asegúrate de que el archivo openapi.yaml esté en la misma carpeta que este
 const swaggerDocument = YAML.load('./openapi.yaml');
-
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: swaggerDocument,
+    validateRequests: true, // Habilita la validación de solicitudes
+    validateResponses: true, // Habilita la validación de respuestas
+    ignorePaths: /docs/,
+  }),
+);  
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: err,
+  });
+});
+
 
 app.get('/hello', (req, res) => {
   res.json({message:'Hello World!'});
